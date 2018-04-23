66706298634377fa8056c9e0188bf8688f77f527
Riley Robertson <rober343@purdue.edu>
2018-04-23 00:19:41 -0400

    Icon Change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import {Container, Row, Col, Button, Jumbotron} from 'reactstrap';
 import logo from './logo.svg';
+import dollar from './dollar.svg';
 import './App.css';
 
 import firebase from './base.js';
@@ -8,9 +10,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+          <Row/>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to our CS252 Project</h1>
+          <img src={dollar} className="App-logo" alt="logo" />
+          <h1 className="App-title">Cash Stash</h1>
         </header>
         <p className="App-intro">
           Team Members:  Jake Koontz, Riley Robertson, Jeremy Putnam

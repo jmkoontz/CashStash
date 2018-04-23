5c3d555008cc8dc391a94b09b80c4a34123882d6
Jake Koontz <jmkoontz@purdue.edu>
2018-04-22 23:28:28 -0400

    Set up basic sign in feature

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { fireauth } from "./base";
+import { Form, FormGroup, Label, Input, Button, Alert } from 'reactstrap';
+
 
 class SignIn extends Component {
   constructor() {
@@ -15,7 +17,30 @@ class SignIn extends Component {
     ev.preventDefault();  // stop page from redirecting
     let self = this;
 
+    fireauth.signInWithEmailAndPassword(ev.target.email.value, ev.target.password.value).catch((error) => {
+      self.setState({
+        errorCode: error.message,
+        visible: true,
+      });
+    });
+  };
 
+  // hide error message
+  onDismiss = () => {
+    this.setState({visible: false});
+  };
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.onFormSubmit()}>
+          <Alert color="danger" isOpen={this.state.visible} toggle={this.onDismiss}>
+            {this.state.errorCode}
+          </Alert>
+          <Button size="lg">Sign In</Button>
+        </Form>
+      </div>
+    )
   }
 }
 

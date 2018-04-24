6b44e0d302441f8edb899993e9e2b3085a632373
Jeremy <jeremyputput@gmail.com>
2018-04-23 23:00:52 -0400

    Made the top bar

--- src/About.js
+++ src/About.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { fireauth } from "./base";
 import { NavLink } from 'react-router-dom';
-import { Form, FormGroup, Label, Input, Button, Alert } from 'reactstrap';
+import { Container, Row, Col, Button, Navbar, Modal, ModalHeader, ModalBody, ModalFooter, Collapse, Nav, NavbarBrand, NavItem, NavbarToggler } from 'reactstrap';
+import './About.css'
+import TopBar from './TopBar';
 
 import logo from './logo.svg';
 
@@ -12,42 +14,28 @@ class About extends Component {
     this.state = {
       errorCode: "",
       visible: false,
+      modal: false,
+      open: true,
     }
+
+    this.toggle = this.toggle.bind(this);
   }
 
-  onFormSubmit = (ev) => {
-    ev.preventDefault();  // stop page from redirecting
-    let self = this;
-
-    fireauth.signInWithEmailAndPassword(ev.target.email.value, ev.target.password.value).catch((error) => {
-      self.setState({
-        errorCode: error.message,
-        visible: true,
-      });
+  toggle() {
+    this.setState({
+      modal: !this.state.modal,
     });
-  };
+  }
 
-  // hide error message
-  onDismiss = () => {
-    this.setState({visible: false});
+  signIn = () => {
+
   };
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to our CS252 Project</h1>
-        </header>
-        <p className="App-intro">
-          Team Members:  Jake Koontz, Riley Robertson, Jeremy Putnam
-        </p>
-        <NavLink to="/CashStash/sign-in">
-          <Button>Sign In</Button>
-        </NavLink>
-        <NavLink to="/CashStash/create-account">
-          <Button>Create Account</Button>
-        </NavLink>
+      <div className={"App"}>
+        <TopBar/>
+
       </div>
     )
   }
---
+++ src/TopBar.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { fireauth } from "./base";
+import { NavLink } from 'react-router-dom';
+import { Container, Row, Col, Button, Navbar, Modal, ModalHeader, ModalBody, ModalFooter, Collapse, Nav, NavbarBrand, NavItem, NavbarToggler, FormGroup, Form, Input, Label, Alert } from 'reactstrap';
+import './TopBar.css'
+import logo from './logo.svg';
+
+class TopBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      errorCode: "",
+      visible: false,
+      modal: false,
+      open: true,
+    }
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  onFormSubmit = (ev) => {
+    ev.preventDefault();  // stop page from redirecting
+    let self = this;
+
+    fireauth.signInWithEmailAndPassword(ev.target.email.value, ev.target.password.value).catch((error) => {
+      self.setState({
+        errorCode: error.message,
+        visible: true,
+      });
+    });
+  };
+
+  // hide error message
+  onDismiss = () => {
+    this.setState({visible: false});
+  };
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  }
+
+  createAccount = () => {
+
+  };
+
+  signIn = () => {
+
+  };
+
+  render() {
+    return (
+      <div className={"App"}>
+        <Navbar className="bg-dark" light expand={"md"}>
+          <NavbarBrand className={"head"}>Cash$tash</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} className="mr-2" />
+          <Collapse isOpen={!this.state.open} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Button onClick={this.toggle}>Sign In</Button>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+
+        <Container fluid>
+          <Row>
+            <Col>
+              <Modal isOpen={this.state.modal} toggle={this.toggle} backdrop={false}>
+                <ModalHeader toggle={this.toggle}>
+                  {/*<img src={logo} alt="" width="50" height="50"/>*/}
+                  Sign In Here!
+                </ModalHeader>
+                <Form onSubmit={(ev) => this.onFormSubmit(ev)}>
+                  <ModalBody>
+                    <FormGroup>
+                      <Input type="email" name="email" id="exampleEmail" placeholder="Email"/>
+                    </FormGroup>
+                    <FormGroup>
+                      <Input type="password" name="password" id="examplePassword" placeholder="Password"/>
+                    </FormGroup>
+                    <Alert color="danger" isOpen={this.state.visible} toggle={this.onDismiss}>
+                      {this.state.errorCode}
+                    </Alert>
+                    <ModalFooter>
+                      <NavLink className={"bgC"} onClick={this.createAccount}>Create Account</NavLink>{' '}
+                      <Button className={"bgC"} onClick={this.signIn}>Sign In</Button>{' '}
+                    </ModalFooter>
+                  </ModalBody>
+                </Form>
+              </Modal>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default TopBar;

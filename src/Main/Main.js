92a9228f14a86de35a6325224d211380dba33065
Riley Robertson <rober343@purdue.edu>
2018-04-24 15:49:49 -0400

    notSignedIn work

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { firestore } from "../base";
 import { Container, Row, Col, Button } from 'reactstrap';
+import {ResponsiveContainer} from 'recharts';
 import './Main.css';
 
 import TopBar from './TopBar';
 import BudgetForm from '../BudgetForm/BudgetForm'
+import FullPie from '../Charts/FullPie'
+import WeekPie from '../Charts/WeeklyPie'
+import DayPie from '../Charts/DailyPie'
 
 import intro from '../dollabills.jpeg'
 
+const vals = [{name: "Living Expense", value: 300, color: "#00C49F"},{name: "Food", value: 250, color: "#55B8D9"},
+    {name: "Luxury", value: 200, color: "#E8F576"},{name: "Entertainment", value: 100, color: "#B855D9"},
+    {name: "Gas", value: 50, color: "#FF8042"}];
+
 class Main extends Component {
+
+
   constructor(props) {
     super(props);
 
@@ -76,7 +86,7 @@ class Main extends Component {
               <Col xs={4}>
                 <h2 className="titleCash">{this.state.firstName}'s Cash Stash</h2>
               </Col>
-              <Col xs={4}/>
+                <Col xs={4}/>
             </Row>
             <Row>
               <Col xs={3}/>
@@ -99,20 +109,75 @@ class Main extends Component {
       )
     } else {  // if user is not signed in
       return (
-        <div className="App">
-          <TopBar signedIn={false} {...data}/>
-          <br/>
-          <br/>
-          <br/>
-          <Row className="space"/>
-          <Row>
-            <Col xs={4}/>
-            <Col xs={4}>
-              <h2 className="titleCash">Cash Stash</h2>
-            </Col>
-            <Col xs={4}/>
-          </Row>
-        </div>
+          <div className="App fullBack">
+
+              <TopBar signedIn={false} {...data}/>
+
+              <Container className="graphBack">
+                  <br/>
+                  <br/>
+                  <br/>
+                  <Row className="space"/>
+                  <Row>
+                      <Col xs={4}/>
+                      <Col xs={4}>
+                          <h2 className="titleCash">Cash Stash</h2>
+                      </Col>
+                      <Col xs={4}/>
+                  </Row>
+                  <hr/>
+                  <Row>
+                      <Col>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <h1 className={"fullBudget"}>Full Budget</h1>
+                      </Col>
+                      <Col>
+                          <FullPie vals={vals}/>
+                      </Col>
+                  </Row>
+                  <hr/>
+                  <Row>
+                      <Col>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <h1 className={"fullBudget"}>Weekly Budget</h1>
+                      </Col>
+                      <Col>
+                          <WeekPie vals={vals}/>
+                      </Col>
+                  </Row>
+                  <hr/>
+                  <Row>
+                      <Col>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <br/>
+                          <h1 className={"fullBudget"}>Daily Budget</h1>
+                      </Col>
+                      <Col>
+                          <DayPie vals={vals}/>
+                      </Col>
+                  </Row>
+              </Container>
+          </div>
       )
     }
   }

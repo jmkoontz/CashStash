b1c06f85087c905f7109363cf63bb792a557f835
Jeremy <jeremyputput@gmail.com>
2018-04-23 23:43:11 -0400

    Added code from the signin and createaccount pages

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { fireauth } from "./base";
+import {fireauth, firestore} from "./base";
 import { NavLink } from 'react-router-dom';
 import { Container, Row, Col, Button, Navbar, Modal, ModalHeader, ModalBody, ModalFooter, Collapse, Nav, NavbarBrand, NavItem, NavbarToggler, FormGroup, Form, Input, Alert } from 'reactstrap';
 import './TopBar.css'
@@ -14,6 +14,8 @@ class TopBar extends Component {
       visible: false,
       modal: false,
       open: true,
+
+      sign: true,
     }
 
   }
@@ -35,25 +37,106 @@ class TopBar extends Component {
     this.setState({visible: false});
   };
 
+  /*
+   * Disables the modal or enables it
+   */
   toggle = () => {
     this.setState({
       modal: !this.state.modal,
     });
-  }
+  };
 
-  createAccount = () => {
+  /*
+   * Creates an account using firebase
+   */
+  createAccount = (ev) => {
+    ev.preventDefault();
     let self = this;
 
     self.setState({
       modal: !this.state.modal,
     });
+
+    let firstName = ev.target.firstName.value;
+    let lastName = ev.target.firstName.value;
+    let email = ev.target.firstName.value;
+
+    if (firstName === "") {
+      self.setState({
+        errorCode: "Please enter your first name",
+        visible: true,
+      });
+    } else if (lastName === "") {
+      self.setState({
+        errorCode: "Please enter your last name",
+        visible: true,
+      });
+    } else {
+      fireauth.createUserAndRetrieveDataWithEmailAndPassword(email, ev.target.password.value).then(() => {
+        self.setState({
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+        });
+
+        self.addUser();
+      }).catch((error) => {
+        self.setState({
+          errorCode: error.message,
+          visible: true,
+        })
+      });
+    }
   };
 
-  signIn = () => {
+  /*
+   * Adds a user to firebase
+   */
+  addUser = () => {
+    let self = this;
+
+    fireauth.onAuthStateChanged((user) => {
+      if (user) {
+        let userRef = firestore.collection("users").doc(user.uid);
+        userRef.set({
+          firstName: self.state.firstName,
+          lastName: self.state.lastName,
+          email: self.state.email,
+        }).catch((error) => {
+          self.setState({
+            errorCode: error.message,
+            visible: true,
+          });
+        });
+      }
+    });
+  };
+
+  /*
+   * Signs you in using firebase
+   */
+  signIn = (ev) => {
+    ev.preventDefault();  // stop page from redirecting
     let self = this;
 
     self.setState({
       modal: !this.state.modal,
+    });
+
+    fireauth.signInWithEmailAndPassword(ev.target.email.value, ev.target.password.value).catch((error) => {
+      self.setState({
+        errorCode: error.message,
+        visible: true,
+      });
+    });
+  };
+
+  /*
+   * Switches between the signin and create account tab
+   */
+  switch = () => {
+    this.setState({
+      sign: !this.state.sign,
     });
   };
 
@@ -76,27 +159,64 @@ class TopBar extends Component {
           <Row>
             <Col>
               <Modal isOpen={this.state.modal} toggle={this.toggle} backdrop={false}>
-                <ModalHeader toggle={this.toggle}>
-                  {/*<img src={logo} alt="" width="50" height="50"/>*/}
-                  Sign In Here
-                </ModalHeader>
-                <Form onSubmit={(ev) => this.onFormSubmit(ev)}>
-                  <ModalBody>
-                    <FormGroup>
-                      <Input type="email" name="email" id="exampleEmail" placeholder="Email"/>
-                    </FormGroup>
-                    <FormGroup>
-                      <Input type="password" name="password" id="examplePassword" placeholder="Password"/>
-                    </FormGroup>
-                    <Alert color="danger" isOpen={this.state.visible} toggle={this.onDismiss}>
-                      {this.state.errorCode}
-                    </Alert>
-                    <ModalFooter>
-                      <Button onClick={this.createAccount}>Create Account</Button>{' '}
-                      <Button onClick={this.signIn}>Sign In</Button>{' '}
-                    </ModalFooter>
-                  </ModalBody>
-                </Form>
+                {this.state.sign
+                  ?
+                  <div>
+                    <ModalHeader toggle={this.toggle}>
+                      {/*<img src={logo} alt="" width="50" height="50"/>*/}
+                      Sign In
+                    </ModalHeader>
+                  <Form onSubmit={(ev) => this.onFormSubmit(ev)}>
+                    <ModalBody>
+                      <FormGroup>
+                        <Input type="email" name="email" id="exampleEmail" placeholder="Email"/>
+                      </FormGroup>
+                      <FormGroup>
+                        <Input type="password" name="password" id="examplePassword" placeholder="Password"/>
+                      </FormGroup>
+                      <Alert color="danger" isOpen={this.state.visible} toggle={this.onDismiss}>
+                        {this.state.errorCode}
+                      </Alert>
+                      <ModalFooter>
+                        <Button onClick={this.switch}>Create Account</Button>{' '}
+                        <Button onClick={(ev) => this.signIn(ev)}>Sign In</Button>{' '}
+                      </ModalFooter>
+                    </ModalBody>
+                  </Form>
+                  </div>
+                  :
+                  <div>
+                    <ModalHeader toggle={this.toggle}>
+                      {/*<img src={logo} alt="" width="50" height="50"/>*/}
+                      Create an account
+                    </ModalHeader>
+                  <Form onSubmit={(ev) => this.onFormSubmit(ev)}>
+                    <ModalBody>
+                      <FormGroup row>
+                        <Col sm={6}>
+                          <Input name="firstName" id="exampleFirstName" placeholder="First Name"/>
+                        </Col>
+                        <Col sm={6}>
+                          <Input name="lastName" id="exampleLastName" placeholder="Last Name"/>
+                        </Col>
+                      </FormGroup>
+                      <FormGroup>
+                        <Input type="email" name="email" id="exampleEmail" placeholder="Email"/>
+                      </FormGroup>
+                      <FormGroup>
+                        <Input type="password" name="password" id="examplePassword" placeholder="Password"/>
+                      </FormGroup>
+                      <Alert color="danger" isOpen={this.state.visible} toggle={this.onDismiss}>
+                        {this.state.errorCode}
+                      </Alert>
+                      <ModalFooter>
+                        <Button onClick={(ev) => this.createAccount(ev)}>Create Account</Button>{' '}
+                        <Button onClick={this.switch}>Sign In</Button>{' '}
+                      </ModalFooter>
+                    </ModalBody>
+                  </Form>
+                  </div>
+                }
               </Modal>
             </Col>
           </Row>

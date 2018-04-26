b3c9e6e376aa8ad1b935b0e8fabb3f0cd93b60cb
Jake Koontz <jmkoontz@purdue.edu>
2018-04-26 12:51:17 -0400

    Fixed final warnings

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,62 +1,29 @@
 import React, {Component} from 'react';
 import {firestore} from "../base";
-import {Container, Row, Col, Button, ListGroup, ListGroupItem} from 'reactstrap';
+import {Container, Row, Col, Button} from 'reactstrap';
 import {ResponsiveContainer} from 'recharts';
 import './Main.css';
 
 import TopBar from './TopBar';
-import BudgetForm from '../BudgetForm/BudgetForm'
-
-//for default values
-import HomeFull from '../Charts/HomeFullPie'
-import HomeWeek from '../Charts/HomeWeeklyPie'
-import HomeDay from '../Charts/HomeDailyPie'
-
-import FullPie from '../Charts/FullPie'
-import WeekPie from '../Charts/WeeklyPie'
-import DayPie from '../Charts/DailyPie'
-
-import HomeLegend from '../HomeLegend'
-import Legend from '../Legend'
-
-import FinalValue from '../FinalValue'
-
+import BudgetForm from '../BudgetForm/BudgetForm';
 import BudgetList from "./BudgetList";
 
+import HomeFull from '../Charts/HomeFullPie';
+import HomeWeek from '../Charts/HomeWeeklyPie';
+import HomeDay from '../Charts/HomeDailyPie';
+import FullPie from '../Charts/FullPie';
+import WeekPie from '../Charts/WeeklyPie';
+import DayPie from '../Charts/DailyPie';
+import HomeLegend from '../HomeLegend';
+import Legend from '../Legend';
+import FinalValue from '../FinalValue';
+
+//for default values
 const vals = [{name: "Living Expense", value: 300, color: "#353941"}, {name: "Food", value: 250, color: "#9cdb97"},
   {name: "Luxury", value: 200, color: "#82c4cc"}, {name: "Entertainment", value: 100, color: "#afa3cc"},
   {name: "Gas", value: 50, color: "#d67b77"}];
 
-/*const colors = [
-  {color: "#353941"},
-  {color: "#9cdb97"},
-  {color: "#82c4cc"},
-  {color: "#afa3cc"},
-  {color: "#d67b77"},
-
-    {color: "#3449a1"},
-    {color: "#9cdff7"},
-    {color: "#82addc"},
-    {color: "#aa445c"},
-    {color: "#d67667"},
-
-    {color: "#35a331"},
-    {color: "#9cffa7"},
-    {color: "#80ffcc"},
-    {color: "#a3a4dc"},
-    {color: "#327"},
-
-    {color: "#aa7741"},
-    {color: "#dda797"},
-    {color: "#8dda7c"},
-    {color: "#afdda7"},
-    {color: "#ddda77"},
-
-];*/
-
 class Main extends Component {
-
-
   constructor(props) {
     super(props);
 
